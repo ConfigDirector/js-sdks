16b521b49154072dcf8281632f7a5a14a4876f49
Alejandro Beiderman <abeiderman@gmail.com>
2026-04-17 11:38:07 -0700

    Fix linter issue

--- packages/vue-sdk/src/client.ts
+++ packages/vue-sdk/src/client.ts
@@ -1,6 +1,6 @@
 import type { ConfigDirectorClient, ConfigDirectorClientOptions } from "@js-browser-client/index";
 import { createBrowserClient } from "@js-browser-client/index";
-import { ConfigDirectorPluginOptions } from "./types";
+import type { ConfigDirectorPluginOptions } from "./types";
 import { createConsoleLogger } from "./logger";
 
 export const createClient = (
