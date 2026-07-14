12ec86d154581294dd4690ddf9fe580c75d61cfe
Alejandro Beiderman <abeiderman@gmail.com>
2026-07-14 12:05:33 -0700

    ci: Add release job for OpenFeature web provider

--- packages/openfeature-web-provider/vitest.config.ts
+++ packages/openfeature-web-provider/vitest.config.ts
@@ -8,7 +8,7 @@ import {
   mswUseSseHandler,
   mswWasRequestReceived,
 } from "../js-browser-client/test/msw-setup";
-import "../js-browser-client/test/vitest-browser-commands.d.ts";
+import "../js-browser-client/test/vitest-browser-commands";
 
 export default defineConfig({
   publicDir: "../../public",
